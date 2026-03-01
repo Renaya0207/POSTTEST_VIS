b6fa1b9e84bb7fd75a9f31e6e0227af146b1db65
naaga <naaga@DESKTOP-8PV5FET>
2026-03-01 23:33:46 +0800

    Add project files.

---
+++ POSTTEST_1/Form1.Designer.vb
@@ -0,0 +1,126 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtIPS = New System.Windows.Forms.TextBox()
+        Me.btnTambah = New System.Windows.Forms.Button()
+        Me.btnReset = New System.Windows.Forms.Button()
+        Me.lblIPK = New System.Windows.Forms.Label()
+        Me.lblPredikat = New System.Windows.Forms.Label()
+        Me.txtIPK = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(246, 196)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(133, 19)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Input IP Semester"
+        '
+        'txtIPS
+        '
+        Me.txtIPS.Location = New System.Drawing.Point(439, 188)
+        Me.txtIPS.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.txtIPS.Name = "txtIPS"
+        Me.txtIPS.Size = New System.Drawing.Size(174, 27)
+        Me.txtIPS.TabIndex = 1
+        '
+        'btnTambah
+        '
+        Me.btnTambah.Location = New System.Drawing.Point(439, 223)
+        Me.btnTambah.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnTambah.Name = "btnTambah"
+        Me.btnTambah.Size = New System.Drawing.Size(84, 27)
+        Me.btnTambah.TabIndex = 2
+        Me.btnTambah.Text = "Tambah"
+        Me.btnTambah.UseVisualStyleBackColor = True
+        '
+        'btnReset
+        '
+        Me.btnReset.Location = New System.Drawing.Point(529, 223)
+        Me.btnReset.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnReset.Name = "btnReset"
+        Me.btnReset.Size = New System.Drawing.Size(84, 27)
+        Me.btnReset.TabIndex = 3
+        Me.btnReset.Text = "Reset"
+        Me.btnReset.UseVisualStyleBackColor = True
+        '
+        'lblIPK
+        '
+        Me.lblIPK.AutoSize = True
+        Me.lblIPK.Location = New System.Drawing.Point(246, 262)
+        Me.lblIPK.Name = "lblIPK"
+        Me.lblIPK.Size = New System.Drawing.Size(98, 19)
+        Me.lblIPK.TabIndex = 4
+        Me.lblIPK.Text = "IP Komulatif"
+        '
+        'lblPredikat
+        '
+        Me.lblPredikat.AutoSize = True
+        Me.lblPredikat.Location = New System.Drawing.Point(504, 318)
+        Me.lblPredikat.Name = "lblPredikat"
+        Me.lblPredikat.Size = New System.Drawing.Size(19, 19)
+        Me.lblPredikat.TabIndex = 5
+        Me.lblPredikat.Text = " -"
+        '
+        'txtIPK
+        '
+        Me.txtIPK.Location = New System.Drawing.Point(439, 262)
+        Me.txtIPK.Name = "txtIPK"
+        Me.txtIPK.ReadOnly = True
+        Me.txtIPK.Size = New System.Drawing.Size(174, 27)
+        Me.txtIPK.TabIndex = 7
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.SkyBlue
+        Me.ClientSize = New System.Drawing.Size(900, 534)
+        Me.Controls.Add(Me.txtIPK)
+        Me.Controls.Add(Me.lblPredikat)
+        Me.Controls.Add(Me.lblIPK)
+        Me.Controls.Add(Me.btnReset)
+        Me.Controls.Add(Me.btnTambah)
+        Me.Controls.Add(Me.txtIPS)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Times New Roman", 10.2!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "Kalkulator Nayul"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtIPS As TextBox
+    Friend WithEvents btnTambah As Button
+    Friend WithEvents btnReset As Button
+    Friend WithEvents lblIPK As Label
+    Friend WithEvents lblPredikat As Label
+    Friend WithEvents txtIPK As TextBox
+End Class
---
+++ POSTTEST_1/Form1.vb
@@ -0,0 +1,59 @@
+﻿Public Class Form1
+
+    Dim totalIP As Double = 0
+    Dim jumlahSemester As Integer = 0
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+
+        Dim ipSemester As Double
+
+        ' Ganti koma jadi titik supaya aman
+        Dim inputAngka As String = txtIPS.Text.Replace(",", ".")
+
+        If Double.TryParse(inputAngka, ipSemester) Then
+
+            If ipSemester >= 0 And ipSemester <= 4 Then
+
+                totalIP += ipSemester
+                jumlahSemester += 1
+
+                Dim ipk As Double = totalIP / jumlahSemester
+
+                txtIPK.Text = ipk.ToString("0.00")
+
+                If ipk >= 2.0 And ipk <= 2.75 Then
+                    lblPredikat.Text = "Cukup"
+                ElseIf ipk >= 2.76 And ipk <= 3.0 Then
+                    lblPredikat.Text = "Memuaskan"
+                ElseIf ipk >= 3.01 Then
+                    lblPredikat.Text = "Sangat Memuaskan"
+                Else
+                    lblPredikat.Text = "-"
+                End If
+
+                txtIPS.Clear()
+                txtIPS.Focus()
+
+            Else
+                MessageBox.Show("IP Semester harus antara 0 - 4")
+            End If
+
+        Else
+            MessageBox.Show("Masukkan angka yang valid!")
+        End If
+
+    End Sub
+
+    Private Sub btnReset_Click(sender As Object, e As EventArgs) Handles btnReset.Click
+
+        totalIP = 0
+        jumlahSemester = 0
+
+        txtIPS.Clear()
+        txtIPK.Clear()
+        lblPredikat.Text = "-"
+        txtIPS.Focus()
+
+    End Sub
+
+End Class
